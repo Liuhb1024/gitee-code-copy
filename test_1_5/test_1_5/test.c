c07d1eda1502ee22cc118aad5965b8058f9e6733
liuhb-clanguage <2353836117@qq.com>
2023-01-05 22:55:18 +0800

    2023寒假代码练习3

---
+++ test_1_5/test_1_5/test.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//明解c语言第二章代码练习
+//2-1运算
+#include <stdio.h>
+
+//int main()
+//{
+//	int vx, vy;
+//	puts("请输入两个整数。");
+//	printf("整数 vx：");
+//	scanf("%d", &vx);
+//	printf("整数 vy：");
+//	scanf("%d", &vy);
+//
+//	printf("vx + vy = %d\n", vx + vy);
+//	printf("vx - vy = %d\n", vx - vy);
+//	printf("vx * vy = %d\n", vx * vy);
+//	printf("vx / vy = %d\n", vx / vy);
+//	printf("vx % vy = %d\n", vx % vy);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a;
+//
+//	printf("请输入一个整数：");
+//	scanf("%d", &a);
+//
+//	printf("最后一位是%d。\n", a % 10);
+//}
+
+//int main()
+//{
+//	int a, b;
+//
+//	puts("请输入两个整数。");
+//	printf("整数a：");
+//	scanf("%d", &a);
+//	printf("整数b：");
+//	scanf("%d", &b);
+//	printf("a除b得%d余%d。", a / b, a % b);
+//	return 0;
+//}
+
+
+
+
+//  想不明白的一个小问题，输出不了64%
+/*
+int main()
+{
+	float a, b;
+	puts("请输入两个整数。");
+	printf("整数a：");
+	scanf("%d", &a);
+	printf("整数b：");
+	scanf("%d", &b);
+
+	printf("a的值是b的%.1f%%", (a / b) * 100);
+	return 0;
+}
+
+
+*/
+
+
+
+
