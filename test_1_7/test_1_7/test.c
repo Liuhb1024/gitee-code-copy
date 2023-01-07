9d273a7cd58a6e2b920ad9af2b5194fdd94a76f6
liuhb-clanguage <2353836117@qq.com>
2023-01-07 22:41:31 +0800

    2023寒假练习代码5

---
+++ test_1_7/test_1_7/test.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//明解c语言第三章小部分代码练习
+#include <stdio.h>
+
+//int main()
+//{
+//	int no;
+//	printf("请输入一个整数：");
+//	scanf("%d", &no);
+//
+//	if (no % 5)
+//		puts("输入的数不能被5整除。");//结果不为0则执行
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int no;
+//	printf("请输入一个整数：");
+//	scanf("%d", &no);
+//
+//	if (no % 2)
+//		puts("输入的数是奇数。");
+//	return 0;
+//}
+
+//int main()
+//{
+//    int no;
+//	printf("请输入一个整数：");
+//	scanf("%d", &no);
+//
+//	if (no % 5)
+//		printf("该整数不能被5整除。");
+//	else
+//		printf("该整数能被5整除。");
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int no;
+//	printf("请输入一个整数：");
+//	scanf("%d", &no);
+//
+//	if (no % 2)
+//		printf("该整数是奇数。");
+//	else
+//		printf("该整数是偶数。");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b;
+//	puts("请输入两个整数。");
+//	printf("整数a：");
+//	scanf("%d", &a);
+//	printf("整数b：");
+//	scanf("%d", &b);
+//
+//	if (a % b)
+//		printf("b不是a的约数。");
+//	else
+//		printf("b是a的约数。");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num;
+//	printf("请输入一个整数：");
+//	scanf("%d", &num);
+//
+//	if (num)
+//		puts("该整数不是0。");
+//	else
+//		puts("该整数是0。");
+//	return 0;
+//}
+
+
+
+
