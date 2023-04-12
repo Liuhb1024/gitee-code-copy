4c3299f49ee6851c0dddcb627c61461cb5f96a42
liuhb-clanguage <2353836117@qq.com>
2023-04-12 22:11:05 +0800

    leetcode27移除数组

---
+++ test_4_12_1/test_4_12_1/leetcode27.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//leetcode27าฦณýสýื้
+int removeElement(int* nums, int numsSize, int val) {
+    int left = 0;
+    for (int right = 0; right < numsSize; right++)
+    {
+        if (nums[right] != val) {
+            nums[left] = nums[right];
+            left++;
+        }
+    }
+    return left;
+}
