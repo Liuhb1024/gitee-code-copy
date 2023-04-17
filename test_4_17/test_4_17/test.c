f03581b218185e7779eaaa409251d06dc6d89b82
liuhb-clanguage <2353836117@qq.com>
2023-04-17 22:47:33 +0800

    自定义2

---
+++ test_4_17/test_4_17/test.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//union Un
+//{
+//	char c;//1
+//	int i;//4
+//};
+//
+//int main()
+//{
+//	union Un u;
+//	printf("%d\n", sizeof(u));
+//	printf("%p\n", &u);
+//	printf("%p\n", &(u.i));
+//	printf("%p\n", &(u.c));
+//
+//	return 0;
+//}
+
+
+
+//union Un
+//{
+//	char c;//1
+//	int i;//4
+//};
+//
+//int main()
+//{
+//	union Un u;
+//	u.i = 1;
+//	if (u.c == 1)
+//		printf("Ð¡¶Ë\n");
+//	else
+//		printf("´ó¶Ë\n");
+//
+//
+//	return 0;
+//}
+
+
+//union Un
+//{
+//	char arr[5];//5
+//	int n;//4
+//};
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(union Un));
+//
+//	return 0;
+//}
+
+//union Un
+//{
+//	short s[7];//14
+//	int n;//4
+//};
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(union Un));
+//
+//	return 0;
+//}
+//
