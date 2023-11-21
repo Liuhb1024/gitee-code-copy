a4ef687f5764454bd793985c216d368049b26b50
liuhb-clanguage <2353836117@qq.com>
2023-11-21 23:02:48 +0800

    leetcode11

---
+++ test_11_21/test_11_21/test.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+/*
+leetcode 11 盛水最多的容器
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1, ret = 0;
+        while(left < right)
+        {
+            int v = min(height[left], height[right]) * (right - left);
+            ret = max(ret, v);
+            if(height[left] < height[right]) left ++ ;
+            else right --;
+        }
+        return ret;
+    }
+};
+
+*/
+
+
